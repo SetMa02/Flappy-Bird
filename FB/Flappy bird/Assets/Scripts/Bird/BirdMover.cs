b55aa3e0fe8d527d2249c341f441881e7390793c
Максим Майнагашев <maxuser99@mail.ru>
2021-08-21 01:17:25 +0700

    animation for bird added

--- FB/Flappy bird/Assets/Scripts/Bird/BirdMover.cs
+++ FB/Flappy bird/Assets/Scripts/Bird/BirdMover.cs
@@ -17,9 +17,12 @@ public class BirdMover : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Quaternion _maxRotation;
     private Quaternion _minRotation;
+    private Animator _animator;
+    private bool _isTaped = false;
     
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
     
         _rigidbody.velocity = Vector2.zero;
@@ -36,7 +39,19 @@ public class BirdMover : MonoBehaviour
             _rigidbody.velocity = new Vector2(_speed, 0);
             transform.rotation = _maxRotation;
             _rigidbody.AddForce(Vector2.up * _tapForce, ForceMode2D.Force);
+            if (_isTaped == false)
+            {
+                _animator.Play("Tap");
+                _isTaped = true;
+            }
+            else
+            {
+                _animator.Play("idle");
+                _isTaped = false;
+            }
+            
         }
         transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+        
     }
 }
