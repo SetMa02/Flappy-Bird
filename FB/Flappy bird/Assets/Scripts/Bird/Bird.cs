2a348735ef8970016dce6e86e8e078f01c1796b9
Максим Майнагашев <maxuser99@mail.ru>
2021-08-22 01:02:19 +0700

    UI Added

--- FB/Flappy bird/Assets/Scripts/Bird/Bird.cs
+++ FB/Flappy bird/Assets/Scripts/Bird/Bird.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BirdMover))]
 public class Bird : MonoBehaviour
 {
     private BirdMover _birdMover;
     private int _score;
+
+    public event UnityAction GameOver;
+
+    public event UnityAction<int> ScoreChanged;
 
     private void Start()
     {
@@ -17,17 +22,18 @@ public class Bird : MonoBehaviour
     public void ResetPlayer()
     {
         _score = 0;
+        ScoreChanged?.Invoke(_score);
         _birdMover.ResetBird();
     }
 
     public void Die()
     {
-        Debug.Log("Dead");
-        Time.timeScale = 0;
+        GameOver?.Invoke();
     }
 
     public void increaseScore()
     {
         _score++;
+        ScoreChanged?.Invoke(_score);
     }
 }
---
+++ FB/Flappy bird/Assets/Scripts/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Bird _bird;
+    [SerializeField] private PipeGenerator _generator;
+    [SerializeField] private StartScreen _startScreen;
+    [SerializeField] private GameOverScreen _gameOver;
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+        _startScreen.Open();
+    }
+
+    private void OnEnable()
+    {
+        _startScreen.PlayButtonClick += OnPlayButtonClick;
+        _gameOver.RestartButtonClick += OnRestartButtonClick;
+        _bird.GameOver += OnGameOver;
+    }
+
+    private void OnDisable()
+    {
+        _startScreen.PlayButtonClick -= OnPlayButtonClick;
+        _gameOver.RestartButtonClick -= OnRestartButtonClick;
+        _bird.GameOver -= OnGameOver;
+    }
+
+    private void OnPlayButtonClick()
+    {
+        _startScreen.Close();
+        StartGame();
+    }
+
+    private void OnRestartButtonClick()
+    {
+        _gameOver.Close();
+        _generator.ResetPool();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        Time.timeScale = 1;
+        _bird.ResetPlayer();
+    }
+
+    public void OnGameOver()
+    {
+        Time.timeScale = 0;
+        _gameOver.Open();
+    }
+}
